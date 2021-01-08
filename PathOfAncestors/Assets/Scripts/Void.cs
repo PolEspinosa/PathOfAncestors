d9148413fbcaa9b44bfd86ab50ced339fd8ec70e
Pol Espinosa <pol.espinosa@enti.cat>
2021-01-08 17:59:19 +0100

    Death and checkpoints

---
+++ PathOfAncestors/Assets/Scripts/Void.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void : MonoBehaviour
+{
+    CheckpointManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("CheckpointManager").GetComponent<CheckpointManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            manager.moveToCheckpoint();
+        }
+    }
+}
