a88a9b088f1b23e545a68eca192c1227ed950bca
Daniel Gallart <daniel.gallart@enti.cat>
2021-01-09 13:27:56 +0100

    torch prefab + fire integrated

---
+++ PathOfAncestors/Assets/Scripts/ActivateFire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFire : MonoBehaviour
+{
+    private Activator activator;
+    public GameObject fire;
+    public GameObject fireLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activator = gameObject.GetComponent<Activator>();
+        fire.SetActive(false);
+        fireLight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activator._activated)
+        {
+            fire.SetActive(true);
+            fireLight.SetActive(true);
+        }
+        else
+        {
+            fire.SetActive(false);
+            fire.SetActive(false);
+        }
+    }
+}
