bbd5fdebdea2366d0876adb77f8dc3630fc06773
Daniel Gallart <daniel.gallart@enti.cat>
2021-01-10 21:59:29 +0100

    burning effect done

--- PathOfAncestors/Assets/Scripts/BurningObject.cs
+++ PathOfAncestors/Assets/Scripts/BurningObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class BurningObject : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class BurningObject : MonoBehaviour
     private float burntAmount;
     public bool burning;
     public float burningSpeed;
+    public VisualEffect fireParticles;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,7 @@ public class BurningObject : MonoBehaviour
         mat = gameObject.GetComponent<MeshRenderer>().material;
         burntAmount = 0;
         burning = false;
+        fireParticles.enabled = false;
     }
 
     // Update is called once per frame
@@ -25,9 +28,16 @@ public class BurningObject : MonoBehaviour
             burntAmount += Time.deltaTime * burningSpeed;
         }
         mat.SetFloat("Vector1_2544001D", burntAmount);
+        //when the object is invisible, destroy it
         if (burntAmount >= 1)
         {
             Destroy(gameObject);
+        }
+        //if there is almost no object left, stop particles
+        else if(burntAmount >= 0.5f)
+        {
+            fireParticles.SetFloat("rate", 0);
+            gameObject.GetComponent<Collider>().enabled = false;
         }
     }
 
@@ -37,6 +47,7 @@ public class BurningObject : MonoBehaviour
         {
             burning = true;
             other.gameObject.GetComponent<BaseSpirit>().MoveTo(other.gameObject.transform.position);
+            fireParticles.enabled = true;
         }
     }
 }
