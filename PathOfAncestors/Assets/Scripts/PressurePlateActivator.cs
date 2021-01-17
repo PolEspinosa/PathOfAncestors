f9944e0e027d297fe843cc11f83a8b0a5852998c
Pol Espinosa <pol.espinosa@enti.cat>
2021-01-17 22:11:35 +0100

    pressureplate

--- PathOfAncestors/Assets/Scripts/PressurePlateActivator.cs
+++ PathOfAncestors/Assets/Scripts/PressurePlateActivator.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class PressurePlateActivator : Activator
 {
+
+    public float pressedPos;
+    public Vector3 startPos;
+    public GameObject pressPlateChild;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        startPos = pressPlateChild.transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (_activated)
+        {
+            if (pressPlateChild.transform.localPosition.y > pressedPos)
+                pressPlateChild.transform.localPosition -= Vector3.up * 0.01f;
+            else if (transform.localPosition.y < pressedPos)
+                pressPlateChild.transform.localPosition = Vector3.up * pressedPos;
+        }
+        else if (!_activated)
+        {
+            if (pressPlateChild.transform.localPosition.y < startPos.y)
+                pressPlateChild.transform.localPosition += Vector3.up * 0.01f;
+
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!_activated)
