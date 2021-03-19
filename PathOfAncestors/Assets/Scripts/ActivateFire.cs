a078a97f0aad7a9c404b6b028bd9de7d8aad04f8
Daniel Gallart <danie.gallart@enti.cat>
2021-03-19 18:53:55 +0100

    .

--- PathOfAncestors/Assets/Scripts/ActivateFire.cs
+++ PathOfAncestors/Assets/Scripts/ActivateFire.cs
@@ -7,26 +7,31 @@ public class ActivateFire : MonoBehaviour
     private Activator activator;
     public GameObject fire;
     public GameObject fireLight;
+    [SerializeField]
+    private bool activated = false;
     // Start is called before the first frame update
     void Start()
     {
         activator = gameObject.GetComponent<Activator>();
-        fire.SetActive(false);
-        fireLight.SetActive(false);
+        fire.SetActive(activated);
+        fireLight.SetActive(activated);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (activator._activated)
+        if (!activated)
         {
-            fire.SetActive(true);
-            fireLight.SetActive(true);
-        }
-        else
-        {
-            fire.SetActive(false);
-            fire.SetActive(false);
+            if (activator._activated)
+            {
+                fire.SetActive(true);
+                fireLight.SetActive(true);
+            }
+            else
+            {
+                fire.SetActive(false);
+                fire.SetActive(false);
+            }
         }
     }
 }
