0f1bfee6d30ff8b7f22593d3a1a68ffed6a7114c
Daniel Gallart <danie.gallart@enti.cat>
2021-03-29 18:19:41 +0200

    ambience wip

---
+++ PathOfAncestors/Assets/Scripts/Audio/AmbienceAudioController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceAudioController : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance indoorAmbienceInstance, outdoorAmbienceInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create indoor ambience instance
+        //indoorAmbienceInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Ambiente/indoorAmbience");
+        //indoorAmbienceInstance.start();
+        //create outdoor ambience instance
+        //outdoorAmbienceInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Ambiente/outdoorAmbience");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
