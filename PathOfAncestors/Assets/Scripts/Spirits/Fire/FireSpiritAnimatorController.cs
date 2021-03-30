74e65172988b516efaeed809022a04a405e15095
Daniel Gallart <danie.gallart@enti.cat>
2021-03-30 22:43:15 +0200

    fire spirit animations wip (idle, invoke, uninvoke)

---
+++ PathOfAncestors/Assets/Scripts/Spirits/Fire/FireSpiritAnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpiritAnimatorController : SpiritsAnimatorController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("invoked", invoked);
+        animator.SetBool("uninvoked", uninvoked);
+    }
+
+    private void FireInvoked()
+    {
+        invoked = true;
+    }
+
+    private void DestroyFire()
+    {
+        destroySpirit = true;
+    }
+}
