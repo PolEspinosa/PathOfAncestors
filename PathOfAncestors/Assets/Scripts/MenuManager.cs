50a69fd889a89244e878997d651f9b7d266d145f
Pol Espinosa <pol.espinosa@enti.cat>
2021-04-08 00:07:18 +0200

    main mennu wip

---
+++ PathOfAncestors/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //start
+    public Button startButton;
+    public Sprite startButtonDeact;
+    public Sprite startButtonAct;
+
+    //exit
+    public Button exitButton;
+    public Sprite exitButtonDeact;
+    public Sprite exitButtonAct;
+
+
+   
+    public void ActiveStart()
+    {
+        startButton.GetComponent<Image>().sprite = startButtonAct;
+    }
+
+    public void DeactiveStart()
+    {
+        startButton.GetComponent<Image>().sprite = startButtonDeact;
+    }
+
+    public void ActiveExit()
+    {
+        exitButton.GetComponent<Image>().sprite = exitButtonAct;
+    }
+
+    public void DeactiveExit()
+    {
+        exitButton.GetComponent<Image>().sprite = exitButtonDeact;
+    }
+
+}
