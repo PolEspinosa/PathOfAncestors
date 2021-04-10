4e4362ffdc959f816e42cfe9bf86612a7a8f6d4a
Pol Espinosa <pol.espinosa@enti.cat>
2021-04-11 01:07:26 +0200

    fixes

--- PathOfAncestors/Assets/Scripts/RotateDoorActivable.cs
+++ PathOfAncestors/Assets/Scripts/RotateDoorActivable.cs
@@ -8,6 +8,8 @@ public class RotateDoorActivable : Activable
 
     public GameObject rightDoor;
     public GameObject leftDoor;
+
+    public bool isRect = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,12 @@ public class RotateDoorActivable : Activable
     {
         rightDoor.transform.DORotateQuaternion(Quaternion.Euler(0, 100, 0), 3f);
         leftDoor.transform.DORotateQuaternion(Quaternion.Euler(0, -100,0), 3f);
+
+        if (isRect)
+        {
+            rightDoor.transform.DORotateQuaternion(Quaternion.Euler(0, -180, 0), 3f);
+            leftDoor.transform.DORotateQuaternion(Quaternion.Euler(0, 10, 0), 3f);
+        }
        
     }
 
