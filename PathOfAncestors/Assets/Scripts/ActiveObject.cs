f4f8fb19986a9a4da07f177aefd05779732516b5
Pol Espinosa <pol.espinosa@enti.cat>
2021-05-04 20:37:23 +0200

    decoration

---
+++ PathOfAncestors/Assets/Scripts/ActiveObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObject : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            foreach(GameObject obj in objects)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+}
