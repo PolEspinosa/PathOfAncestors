a1ed776531d16cb71bfac5c92406169130788f5e
Daniel Gallart <danie.gallart@enti.cat>
2021-05-09 13:01:51 +0200

    player prefs

---
+++ PathOfAncestors/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public static float masterVolume, musicVolume, ambienceVolume, sfxVolume;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        LoadData();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SaveData();
+        }
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetFloat("masterVolume", masterVolume);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.SetFloat("ambienceVolume", ambienceVolume);
+        PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadData()
+    {
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            masterVolume = PlayerPrefs.GetFloat("masterVolume");
+            musicVolume = PlayerPrefs.GetFloat("musicVolume");
+            ambienceVolume = PlayerPrefs.GetFloat("ambienceVolume");
+            sfxVolume = PlayerPrefs.GetFloat("sfxVolume");
+        }
+        else
+        {
+            masterVolume = 1f;
+            musicVolume = 1f;
+            ambienceVolume = 1f;
+            sfxVolume = 1f;
+        }
+    }
+}
--- PathOfAncestors/Assets/Scripts/Menu/OptionsMenu.cs
+++ PathOfAncestors/Assets/Scripts/Menu/OptionsMenu.cs
@@ -9,6 +9,8 @@ public class OptionsMenu : MonoBehaviour
 
     //volume variables
     private float masterVolume, musicVolume, ambienceVolume, SFXVolume;
+    [SerializeField]
+    private Slider masterSlider, musicSlider, ambienceSlider, sfxSlider;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,21 @@ public class OptionsMenu : MonoBehaviour
         musicMixer = FMODUnity.RuntimeManager.GetBus("bus:/Master/music");
         ambienceMixer = FMODUnity.RuntimeManager.GetBus("bus:/Master/ambient");
         sfxMixer = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+
+        masterVolume = GameManager.masterVolume;
+        musicVolume = GameManager.musicVolume;
+        ambienceVolume = GameManager.ambienceVolume;
+        SFXVolume = GameManager.sfxVolume;
+
+        masterSlider.value = masterVolume;
+        musicSlider.value = musicVolume;
+        ambienceSlider.value = ambienceVolume;
+        sfxSlider.value = SFXVolume;
+
+        masterMixer.setVolume(masterVolume);
+        musicMixer.setVolume(musicVolume);
+        ambienceMixer.setVolume(ambienceVolume);
+        sfxMixer.setVolume(SFXVolume);
     }
 
     // Update is called once per frame
@@ -28,20 +45,24 @@ public class OptionsMenu : MonoBehaviour
     public void SetMasterVolume(float _volume)
     {
         masterMixer.setVolume(_volume);
+        GameManager.masterVolume= _volume;
     }
 
     public void SetMusicVolume(float _volume)
     {
         musicMixer.setVolume(_volume);
+        GameManager.musicVolume = _volume;
     }
 
     public void SetAmbienceVolume(float _volume)
     {
         ambienceMixer.setVolume(_volume);
+        GameManager.ambienceVolume = _volume;
     }
 
     public void SetSFXVolume(float _volume)
     {
         sfxMixer.setVolume(_volume);
+        GameManager.sfxVolume = _volume;
     }
 }
