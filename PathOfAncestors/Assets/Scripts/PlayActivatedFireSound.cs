23462a603ffa5b01240bb41400e4b504a06a5555
Daniel Gallart <daniel.gallart@enti.cat>
2021-05-16 21:50:47 +0200

    fire sound try

---
+++ PathOfAncestors/Assets/Scripts/PlayActivatedFireSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayActivatedFireSound : MonoBehaviour
+{
+    [SerializeField]
+    private Activator activator;
+    //create the event instance for the torch sound
+    private FMOD.Studio.EventInstance torchSoundInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        torchSoundInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Puerta 2/openBigDoor");
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(torchSoundInstance, gameObject.transform, gameObject.GetComponent<Rigidbody>());
+        //if (activator._activated)
+        //{
+        //    torchSoundInstance.start();
+        //}
+        
+        torchSoundInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
