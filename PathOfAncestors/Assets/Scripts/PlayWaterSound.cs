7ee7ef3d58fda75b28f84a087be808c1ebf7f599
Daniel Gallart <daniel.gallart@enti.cat>
2021-05-17 00:15:55 +0200

    water sound

---
+++ PathOfAncestors/Assets/Scripts/PlayWaterSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWaterSound : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance waterSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterSound = FMODUnity.RuntimeManager.CreateInstance("event:/Props/waterSound");
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(waterSound, gameObject.transform, gameObject.GetComponent<Rigidbody>());
+        waterSound.setVolume(0.15f);
+        waterSound.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
